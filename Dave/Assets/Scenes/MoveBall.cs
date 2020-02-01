3b3d0935f16371bff2c6eb22ebb9c47bef089057
Peter Blanchfield <5593832+drbatnott@users.noreply.github.com>
2020-02-01 13:57:58 +0000

    Demo Unity For SPM Added
    Added the Dave Project which is the beginnig of what we were doing

---
+++ Dave/Assets/Scenes/MoveBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(h, 0f, v), ForceMode.Impulse);
+    }
+}
